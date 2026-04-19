eef3768dad8bad147fc130513e1c47c6919a6fa2
Arvind <pingarvindforquestions@gmail.com>
2026-04-19 22:18:27 +0530

    build(prettier): updaeted prettier config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+const prettierConfig = {
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+};
+
+export default prettierConfig;
